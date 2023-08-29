3a441eb4c9cbd2a78acb4e0a9651dbc9e44d3397
Carlos Damian <carlos.damian@choicehotels.com>
2023-08-28 20:18:51 -0600

    fet(theme): added theme provider

---
+++ src/app/context/ThemeContextProvider.tsx
@@ -0,0 +1,7 @@
+'use client';
+import { ThemeProvider } from 'next-themes';
+import React, { ReactNode } from 'react';
+
+export const ThemeContextProvider = ({ children }: { children: ReactNode }) => {
+  return <ThemeProvider enableSystem={true}>{children}</ThemeProvider>;
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Plus_Jakarta_Sans } from 'next/font/google';
+import { ThemeContextProvider } from './context/ThemeContextProvider';
 
 const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'] });
 
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={jakarta.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning={true}>
+      <body className={jakarta.className}>
+        <ThemeContextProvider>{children}</ThemeContextProvider>
+      </body>
     </html>
   );
 }
