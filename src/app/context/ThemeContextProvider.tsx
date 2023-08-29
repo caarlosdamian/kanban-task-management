8679b7975e634917add1870497b29243d51fc8ae
caarlosdamian <dapecarlos@gmail.com>
2023-08-28 23:02:15 -0600

    feat(theme): added theme functionality

--- src/app/context/ThemeContextProvider.tsx
+++ src/app/context/ThemeContextProvider.tsx
@@ -3,5 +3,5 @@ import { ThemeProvider } from 'next-themes';
 import React, { ReactNode } from 'react';
 
 export const ThemeContextProvider = ({ children }: { children: ReactNode }) => {
-  return <ThemeProvider enableSystem={true}>{children}</ThemeProvider>;
+  return <ThemeProvider enableSystem={true} >{children}</ThemeProvider>;
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
-'use client';
-import { ThemeProvider } from 'next-themes';
-import { useTheme } from 'next-themes';
-import Image from 'next/image';
-
 export default function Home() {
-  const {theme} = useTheme();
-  console.log('===ThemeContextProvider==',theme)
-  return (
-    <main className="bg-content min-w-screen min-h-screen">
-      <h1 className="text-secondary">Hello</h1>
-    </main>
-  );
+  return <main className="bg-primary min-w-screen min-h-screen"></main>;
 }
