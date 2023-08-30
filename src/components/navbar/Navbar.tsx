f2b4898df87ae5ff9de650d31d5123d26d20d88c
caarlosdamian <dapecarlos@gmail.com>
2023-08-30 16:05:49 -0600

    feat(navbar): added navbar styling

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -9,13 +9,13 @@ export const Navbar = () => {
   //
   return (
     <nav className="flex items-center gap-4 bg-primary  border-b border-lines">
-      <div className="md:border-r md:border-lines w-full pl-4 py-5 md:pt-7 md:px-6 min-w-[260px] max-w-[300px] flex-1">
+      <div className=" pl-4 py-5  md:w-full md:py-7 md:px-6 md:min-w-[260px] max-w-[300px] md:flex-1">
         <Image
           src="/logo-mobile.svg"
           alt="logo-mobile"
           width={24}
           height={24}
-          className="md:hidden "
+          className="md:hidden"
         />
         <Image
           src={`/logo-${resolvedTheme === 'dark' ? 'light' : 'dark'}.svg`}
@@ -25,7 +25,7 @@ export const Navbar = () => {
           className="hidden md:block"
         />
       </div>
-      <div className="flex items-center justify-between w-full  pr-4 py-5 md:px-6 md:py-7">
+      <div className="flex items-center justify-between w-full  pr-4 py-5 md:px-6 md:py-7 md:border-l md:border-lines lg:px-8">
         <div className="flex gap-2 items-center cursor-pointer">
           <h1 className="text-base font-semibold md:text-xl">
             Platform Launch
@@ -38,8 +38,8 @@ export const Navbar = () => {
             className="md:hidden"
           />
         </div>
-        <div className="flex items-center gap-4">
-          <div className="bg-mainPurpleHover opacity-50 px-18px p-[10px] flex-[2] rounded-3xl md:hidden">
+        <div className="flex items-center gap-4 md:gap-6">
+          <div className="bg-mainPurpleHover opacity-50 px-18px p-[10px] md:flex-[2] rounded-3xl md:hidden">
             <Image
               src="/icon-add-task-mobile.svg"
               alt="logo-mobile"
@@ -51,13 +51,14 @@ export const Navbar = () => {
           <Button
             size="lg"
             label="+ Add New Task"
-            className="hidden md:block"
+            className="hidden md:block md:px-6"
           />
           <Image
             src="/icon-vertical-ellipsis.svg"
             alt="logo-mobile"
             width={4}
             height={16}
+            className="cursor-pointer"
           />
         </div>
       </div>
