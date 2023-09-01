15b807ad1bdd2c326414240d972f623fe0cefb44
caarlosdamian <dapecarlos@gmail.com>
2023-08-31 20:14:29 -0600

    feat(sidebar): added new sidebar logic in progress;

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
-export const Sidebar = () => {
-  return (
-    <aside className="h-[322px] w-[264px] shadow-2xl md:shadow-none md:h-screen bg-primary md:max-w-[300px] md:min-[260px]">
+const content = (
+  <aside className="h-[322px] w-[264px] shadow-2xl md:shadow-none md:h-screen bg-primary md:max-w-[300px] md:min-[260px]">
+    <div className="">
+      <p></p>
       <div className="">
-        <p></p>
-        <div className="">
-          <p>tags</p>
-        </div>
+        <p>tests</p>
       </div>
-      <div className=""></div>
-    </aside>
-  );
+    </div>
+    <div className=""></div>
+  </aside>
+);
+
+export const Sidebar = () => {
+  return <>{content}</>;
 };
